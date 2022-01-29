ee6726234eef61147165bc69fdbf0e976e697e58
kimyoulim <kimyoulim1016@gmail.com>
2022-01-29 20:15:21 +0900

    api적용 및 swagger test완료

--- src/main/java/com/project/noteking/web/board/domain/Board.java
+++ src/main/java/com/project/noteking/web/board/domain/Board.java
@@ -1,17 +1,25 @@
 package com.project.noteking.web.board.domain;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.sql.Date;
 
 @Data
 public class Board {
+  @ApiModelProperty(notes = "게시물 번호", example = "auto_increment")
   private int board_id;
+  @ApiModelProperty(notes = "회원 번호")
   private int user_id;
+  @ApiModelProperty(notes = "글 제목", example = "TEXT")
   private String title;
+  @ApiModelProperty(notes = "글 내용", example = "TEXT")
   private String note;
+  @ApiModelProperty(notes = "사진", example = "TEXT")
   private String img;
-  private Date edit_datee;
-  private Date date;
+  @ApiModelProperty(notes = "수정 날짜", example = "DATE")
+  private String edit_date;
+  @ApiModelProperty(notes = "작성 날짜", example = "DATE")
+  private String date;
 
 }
