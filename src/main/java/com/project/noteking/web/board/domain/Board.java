6f7878985cdb95beddf1d77e96eb851d4580f507
kimyoulim <kimyoulim1016@gmail.com>
2022-02-06 19:50:01 +0900

    aws 연동 완료 및 add준비 완료

--- src/main/java/com/project/noteking/web/board/domain/Board.java
+++ src/main/java/com/project/noteking/web/board/domain/Board.java
@@ -8,19 +8,19 @@ import java.sql.Date;
 
 @Data
 public class Board {
-  @ApiModelProperty(notes = "게시물 번호", example = "auto_increment")
+  @ApiModelProperty(notes = "게시물 번호")
   private int board_id;
-  @ApiModelProperty(notes = "회원 번호", example = "int")
+  @ApiModelProperty(notes = "회원 번호")
   private int user_id;
-  @ApiModelProperty(notes = "글 제목", example = "String")
+  @ApiModelProperty(notes = "글 제목")
   private String title;
-  @ApiModelProperty(notes = "글 내용", example = "String")
+  @ApiModelProperty(notes = "글 내용")
   private String note;
-  @ApiModelProperty(notes = "사진", example = "String")
+  @ApiModelProperty(notes = "사진")
   private String img;
-  @ApiModelProperty(notes = "수정 날짜", example = "String")
+  @ApiModelProperty(notes = "수정 날짜")
   private String edit_date;
-  @ApiModelProperty(notes = "작성 날짜", example = "String")
+  @ApiModelProperty(notes = "작성 날짜")
   private String date;
 
   @ApiModelProperty(notes = "맴버 정보")
